3f2e523eb520324305cbd4a68a02830165a56ae6
ajaysmartsafex <ajaysmartsafex@gmail.com>
2025-05-06 09:58:01 +0530

    update code

--- javascript-object/showUniqueItems.js
+++ javascript-object/showUniqueItems.js
@@ -3,16 +3,16 @@ const array  = [1,2,1,3,5,2];
 // console.log(DublicateItem = [...new Set(array)]); 
 
 
-// function removeDublicateElement(array) {    
-//      return array.reduce((acc, CurEle) => {      
-//       if(!acc.includes(CurEle)){
-//         acc.push(CurEle)        
-//       }      
-//       return acc;      
-//     }, [])
-//  }
+function removeDublicateElement(array) {    
+     return array.reduce((acc, CurEle) => {      
+      if(!acc.includes(CurEle)){
+        acc.push(CurEle)        
+      }      
+      return acc;      
+    }, [])
+ }
 
-// console.log(removeDublicateElement(array));
+console.log(removeDublicateElement(array));
 
 
- console.log(array.filter((item, index) => array.indexOf(item) === index)); 
+//  console.log(array.filter((item, index) => array.indexOf(item) === index)); 
