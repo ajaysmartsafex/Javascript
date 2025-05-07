97e3ebbb907f9f67b2cf863731e001cb18053ae8
ajaysmartsafex <ajaysmartsafex@gmail.com>
2025-05-07 12:29:13 +0530

    use reduser hook function

---
+++ react/useReduser.js
@@ -0,0 +1,30 @@
+// useReducer.js
+
+import React, { useReducer } from 'react';
+
+function Counter() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    
+    const initialState = { count: 0 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+  return (
+    <div>
+      <h2>Count: {state.count}</h2>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </div>
+  );
+}
+
+export default Counter;
